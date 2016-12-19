aa96713a96c400704d634ba104a36d4ea1055556
brecabal <belenrecabal@gmail.com>
2016-12-19 18:59:21 -0300

    Etapa 8, agregando funcionalidad click para desplegar o mostrar seccion agregando clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,14 @@
 $(document).ready( function(){
 	$('.js-menu').hide();
 	console.log('Documento HTML está listo');
+
+	$('.js-show-make').on('click', function(){
+ 		$('.page').addClass('make');
+ 	});
+	
+	$('.js-show-recipe').on('click', function(){
+ 		$('.page').removeClass('make');
+ 	});
+ 
+ 	
 });
